3df02e0bfa2482efc220abf6143d3aeef46cefed
zbahati <bahatizirimwabagabo@gmail.com>
2024-07-16 22:16:51 +0200

    Add dto of users

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,19 @@
-export class CreateUserDto {}
+import { IsNotEmpty, IsOptional, IsString } from "class-validator";
+
+export class CreateUserDto {
+    
+    @IsNotEmpty()
+    @IsString()
+    name: string
+
+    @IsNotEmpty()
+    @IsString()
+    email: string
+
+    @IsNotEmpty()
+    @IsString()
+    password: string
+
+    @IsOptional()
+    role: string
+}
