19444c2a947793fa68a53cd6bd905463c49b52f4
zbahati <bahatizirimwabagabo@gmail.com>
2024-07-16 22:13:01 +0200

    start working on users module

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,26 @@
-export class User {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, Timestamp, UpdateDateColumn } from "typeorm";
+import { Roles } from "../enum/roles.enum";
+
+@Entity('users')
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    name: string
+    
+    @Column()
+    email: string
+
+    @Column()
+    password: string
+
+    @Column({type: 'enum', enum: Roles, array: true, default: [Roles.ADMIN]})
+    role: string
+
+    @CreateDateColumn()
+    created_at: Timestamp
+
+    @UpdateDateColumn()
+    updated_at: Timestamp
+}
